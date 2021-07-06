40c56663cd6864a073af36b9943781320906c4c2
shoaib ahamed <shoaibahamedshafi@gmail.com>
2021-07-06 13:28:04 +0600

    homepages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    "BASE_URL" : "https://travel-blog-nu.vercel.app/",
+    "BASE_URL" : "http://localhost:3000",
     "MONGODB_URL" : "mongodb+srv://shoaibahamed:shoaibahamed@cluster0.hkt9w.mongodb.net/e-shop?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET" : "shoaibahamedshafi@gmail.com01672019292",
     "REFRESH_TOKEN_SECRET" : "shoaibahamedshafi@gmail.com0167201929201303082197",
